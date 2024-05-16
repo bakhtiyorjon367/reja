2488ba08f8662111f962cbe84ba98a82d4f35063
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-05-16 20:43:53 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -9,33 +9,47 @@ console.log("Web Serverni boshlash");
 const express = require('express');
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if(err){
+        console.log("ERROR : ",err)
+    }else{
+        user=JSON.parse(data)
+    }
+});
 
 //express we server 4 bosqichi
-//#1 birinchi bosqish                 -->  expressga kirib kelyotgan malumotlarga bog'liq bo'lgan codelar yoziladi.
-app.use(express.static("public")); // -->  xar-qanday browserdan kirib kelyotgan zaproslar uchun "public folder" ochiq degan manoni anglatadi. (faqat public folderini ko'roladi )
-app.use(express.json());          //  -->  kirib kelyotgan json formatidagi datani object xolatiga o'girib beradi.
-app.use(express.urlencoded({ extended: true })); // --> HTML formdan post qilinga narsalarni qabul qilib oladi.
+//#1 Kirish qismi                                          -->  expressga kirib kelyotgan malumotlarga bog'liq bo'lgan codelar yoziladi.
+app.use(express.static("public"));                      // -->  xar-qanday browserdan kirib kelyotgan zaproslar uchun "public folder" ochiq degan manoni anglatadi. (faqat public folderini ko'roladi )
+app.use(express.json());                               //  -->  kirib kelyotgan json formatidagi datani object xolatiga o'girib beradi.
+app.use(express.urlencoded({ extended: true }));        // --> HTML formdan post qilinga narsalarni qabul qilib oladi.
 
 
 //#2 Sessionlarga bog'liq bo'lgan express codelar
 
 //#3 View code (front-end)
-app.set("views", "views");            // --> folder ko'rsatilyabdi
-app.set("view engine", "ejs");      // --> view engin "ejs" ekanligini ko'rsatish
+app.set("views", "views");                         // --> folder ko'rsatilyabdi
+app.set("view engine", "ejs");                     // --> view engin "ejs" ekanligini ko'rsatish
 
 
 //#4 Routing codes (Routerlarga mo'ljallangan)
-app.post("/create-item", (req, res) =>{
+app.post("/create-item", (req, res) =>{              //malumotni o'zi bilan olib keladi va databsega yozadi
     console.log(req.body);
     res.json({test:"success"});
 });
-app.get("/", function (req, res) {
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user} )
+});
+
+app.get("/", function (req, res) {                   //databasedan malumotni olish/o'qish uchun get ishlatiladi
     res.render("harid");
 });
 
 
-const server = http.createServer(app);       // --> serverni quirish. (createServer bir parametr qabul qiladi. Bu biz yasab olgan express app)
-const PORT = 3000;                             //--> serverni malum bir portga listen qildirish 
+const server = http.createServer(app);                // --> serverni quirish. (createServer bir parametr qabul qiladi. Bu biz yasab olgan express app)
+const PORT = 3000;                                   //--> serverni malum bir portga listen qildirish 
 server.listen(PORT, function () {   
     console.log(`The server is running succesfully on PORT ${PORT}`) }); // --> server muvoffaqiyatlik ishlasa bu console.log ishga tushadi                                                 
